874403ac17cd111792121591f643b7083b297685
xbony2 <xbony22@gmail.com>
2015-02-20 19:52:50 -0500

    this is going nicely :)

--- graveyard-library-clj/src/graveyard_library_clj/math/temperature_converter.clj
+++ graveyard-library-clj/src/graveyard_library_clj/math/temperature_converter.clj
@@ -3,3 +3,23 @@
 (defn fahrenheit-to-celsius
   [fah]
   (* (/ 5 9) (- fah 32)))
+
+(defn fanrenheit-to-kelvin
+  [fah]
+  (* (/ 5 9) (+ fan 459.67)))
+
+(defn celsius-to-fahrenheit
+  [cel]
+  (+ 32 (* cel (/ 9 5))))
+
+(defn celsius-to-kelvin
+  [cel]
+  (+ cel 273.15))
+
+(defn kelvin-to-fahrenheit
+  [kelv]
+  (- (* kelv (9/5)) 459.67))
+
+(defn kelvin-to-celsius
+  [kelv]
+  (- kelv 273.15))
